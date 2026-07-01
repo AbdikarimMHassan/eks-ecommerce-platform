294d13b9aebe2879472d3782503cecf55d70b682
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-07-01 20:10:43 +0100

    wire in argocd and karpenter to prod main

--- eks-ecommerce-platform/infra/environments/prod/main.tf
+++ eks-ecommerce-platform/infra/environments/prod/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
   }
 }
 
@@ -12,9 +16,21 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-module "networking" {
-  source = "../../modules/networking"
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    }
+  }
+}
 
+module "networking" {
+  source              = "../../modules/networking"
+  region              = "eu-west-2"
   environment         = local.environment
   cluster_name        = local.cluster_name
   vpc_name            = local.vpc_name
@@ -23,3 +39,26 @@ module "networking" {
   private_app_subnets = local.private_app_subnets
 }
 
+module "eks" {
+  source              = "../../modules/eks"
+  cluster_name        = local.cluster_name
+  environment         = local.environment
+  vpc_id              = module.networking.vpc_id
+  private_subnet_ids  = module.networking.private_subnet_ids
+  public_subnet_ids   = module.networking.public_subnet_ids
+  admin_iam_arn       = local.admin_iam_arn
+  ci_pipeline_iam_arn = local.ci_pipeline_iam_arn
+}
+
+module "karpenter" {
+  source            = "../../modules/karpenter"
+  cluster_name      = local.cluster_name
+  environment       = local.environment
+  oidc_provider_arn = module.eks.oidc_provider_arn
+}
+
+module "argocd" {
+  source            = "../../modules/argocd"
+  cluster_endpoint  = module.eks.cluster_endpoint
+  high_availability = true
+}
