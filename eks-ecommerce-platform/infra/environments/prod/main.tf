d6416492ae09ea4acd71b218c78ec22c5ace20f7
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-09-05 13:41:40 +0100

    remove duplicate providers in prod  main.tf

--- eks-ecommerce-platform/infra/environments/prod/main.tf
+++ eks-ecommerce-platform/infra/environments/prod/main.tf
@@ -1,33 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
-    }
-  }
-}
-
 module "networking" {
   source              = "../../modules/networking"
   region              = "eu-west-2"
