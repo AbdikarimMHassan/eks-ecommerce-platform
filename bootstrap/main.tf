63a443171c290c7daf3bfacdd215a45dc0861be0
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-09-15 03:13:34 +0100

    add github oidc for the infra and image pipelines

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,11 +4,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  github_repo = "AbdikarimMHassan/eks-ecommerce-platform"
 }
 
 resource "aws_s3_bucket" "state_bucket" {
@@ -38,4 +48,97 @@ resource "aws_s3_bucket_public_access_block" "state_public_block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+data "aws_iam_policy_document" "github_actions_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${local.github_repo}:*"]
+    }
+  }
+}
+
+
+resource "aws_iam_role" "github_actions_eks_deployer" {
+  name               = "github-actions-eks-deployer"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "deployer_admin" {
+  role       = aws_iam_role.github_actions_eks_deployer.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# --- App image-build pipeline role ---
+# Narrow on purpose: each service's build workflow only needs to push its
+# image to ECR, not touch IAM/EKS/VPC. Scoped to the "prod/*" repos created
+# by infra/modules/ecr.
+
+resource "aws_iam_role" "github_actions_ecr_push" {
+  name               = "github-actions-ecr-push"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume.json
+}
+
+data "aws_iam_policy_document" "ecr_push" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:BatchGetImage",
+      "ecr:DescribeRepositories",
+    ]
+    resources = ["arn:aws:ecr:eu-west-2:${data.aws_caller_identity.current.account_id}:repository/prod/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name   = "ecr-push"
+  role   = aws_iam_role.github_actions_ecr_push.id
+  policy = data.aws_iam_policy_document.ecr_push.json
+}
+
+output "github_actions_eks_deployer_role_arn" {
+  value = aws_iam_role.github_actions_eks_deployer.arn
+}
+
+output "github_actions_ecr_push_role_arn" {
+  value = aws_iam_role.github_actions_ecr_push.arn
 }
