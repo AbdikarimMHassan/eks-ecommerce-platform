5edc17e3d2bec451a6e29b7c8d357c03487dc0fe
abdikarimmhassan <abdikarimmh98@gmail.com>
2026-09-16 01:26:03 +0100

    update providers version

--- infra/environments/prod/providers.tf
+++ infra/environments/prod/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
@@ -13,6 +13,10 @@ terraform {
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.14"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
     }
   }
 
@@ -57,5 +61,24 @@ provider "helm" {
         local.region
       ]
     }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      local.region
+    ]
   }
 }
